5fd8a1733b2453ccd8d66a3be39aad0122ef45b3
Patrick Archambeau <patrickarchambeau@gmail.com>
2015-10-24 21:53:37 -0700

    author blockchain wallet receive address now

--- app.py
+++ app.py
@@ -9,11 +9,17 @@ app = Flask(__name__)
 def hello():
     print 'Hitting main route'
 
+    wallet_id = 'c4e7c925-8a48-4a0f-bf0a-88bae5def1cb'
+    wallet_password = 'Loeras2662#s2n'
+
+    author_receive_address = '1JGcSP5qu5BZG7gtt8rEaob9nxBxWaJzAz'
+    reader_send_from_address = '1HKGvNjrMuZ71jkHAFA6e77iPxWM1pranx'
+
     # Create new wallet Ghetto ass credentials, need to get a "FROM ADDRESS from an identifier"
-    wallet = Wallet('c4e7c925-8a48-4a0f-bf0a-88bae5def1cb', 'Loeras2662#s2n')
+    wallet = Wallet(wallet_id, wallet_password)
 
     # Send from wallet to new address (Receiving address (My Multibit HD for testing), Amount in satoshi, from address)
-    payment = wallet.send('1GKjg6fJjFFjRfJSbSzPSzxzaZZ2iwX1aJ', 2000, from_address='1HKGvNjrMuZ71jkHAFA6e77iPxWM1pranx')
+    payment = wallet.send(author_receive_address, 2000, from_address=reader_send_from_address)
 
     print payment.tx_hash
 
