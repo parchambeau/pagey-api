67feeb7672e7fd3f5c2627f55da0ca8fcd8d3065
Patrick Archambeau <patrickarchambeau@gmail.com>
2015-10-24 22:10:33 -0700

    adding in balance call

--- app.py
+++ app.py
@@ -5,23 +5,28 @@ from blockchain.wallet import Wallet
 
 app = Flask(__name__)
 
+reader_wallet_id = 'c4e7c925-8a48-4a0f-bf0a-88bae5def1cb'
+reader_wallet_password = 'Loeras2662#s2n'
+reader_send_from_address = '1HKGvNjrMuZ71jkHAFA6e77iPxWM1pranx'
+
+author_receive_address = '1JGcSP5qu5BZG7gtt8rEaob9nxBxWaJzAz'
+
 @app.route('/')
 def send_btc_to_author():
-    print 'Sending 2000 bits from reader to author for turning page'
-
-    wallet_id = 'c4e7c925-8a48-4a0f-bf0a-88bae5def1cb'
-    wallet_password = 'Loeras2662#s2n'
-
-    author_receive_address = '1JGcSP5qu5BZG7gtt8rEaob9nxBxWaJzAz'
-    reader_send_from_address = '1HKGvNjrMuZ71jkHAFA6e77iPxWM1pranx'
+    print 'Attempting to send 2000 bits from reader to author for turning page'
 
     # Set Wallet ojbect to send from
-    wallet = Wallet(wallet_id, wallet_password)
+    wallet = Wallet(reader_wallet_id, reader_wallet_password)
 
     # Send from wallet to new address (Receiving address (AUTHOR), Amount in satoshi, from address (READER))
     payment = wallet.send(author_receive_address, 2000, from_address=reader_send_from_address)
 
-    print 'Transaction Hash to search blockchain.info to verify transactions: ' + str(payment.tx_hash)
+    if payment != None:
+        print 'Sent 2000 bits from reader to author for turning page'
+        print 'Transaction Hash to search blockchain.info to verify transactions: ' + str(payment.tx_hash)
+
+    else:
+        print 'Something weird happened, payment object is null / none'
 
     return
 
@@ -29,7 +34,19 @@ def send_btc_to_author():
 def check_balance():
     print 'Checking balance of wallet...'
 
-    return
+    # Make sure to grab wallet of reader
+    wallet = Wallet(reader_wallet_id, reader_wallet_password)
+
+    # Pull back the balance for readers wallet
+    balance = wallet.get_balance()
+
+    if balance != None:
+        print 'Current total balance of readers wallet is ' + str(balance)
+
+    else:
+        print 'Balance not being properly returned...'
+
+    return balance
 
 
 if __name__ == "__main__":
