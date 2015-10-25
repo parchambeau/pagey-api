3aaf90a68f2b29ec0f167539e93b6f203a13af9b
Patrick Archambeau <patrickarchambeau@gmail.com>
2015-10-24 21:55:56 -0700

    clean

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    print 'Hitting main route'
+    print 'Sending 2000 bits from reader to author for turning page'
 
     wallet_id = 'c4e7c925-8a48-4a0f-bf0a-88bae5def1cb'
     wallet_password = 'Loeras2662#s2n'
@@ -15,13 +15,13 @@ def hello():
     author_receive_address = '1JGcSP5qu5BZG7gtt8rEaob9nxBxWaJzAz'
     reader_send_from_address = '1HKGvNjrMuZ71jkHAFA6e77iPxWM1pranx'
 
-    # Create new wallet Ghetto ass credentials, need to get a "FROM ADDRESS from an identifier"
+    # Set Wallet ojbect to send from
     wallet = Wallet(wallet_id, wallet_password)
 
-    # Send from wallet to new address (Receiving address (My Multibit HD for testing), Amount in satoshi, from address)
+    # Send from wallet to new address (Receiving address (AUTHOR), Amount in satoshi, from address (READER))
     payment = wallet.send(author_receive_address, 2000, from_address=reader_send_from_address)
 
-    print payment.tx_hash
+    print 'Transaction Hash to search blockchain.info to verify transactions: ' + str(payment.tx_hash)
 
     return
 
