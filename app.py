4178ce9d5d869d883b6d437de9349335481fe9ca
Patrick Archambeau <patrickarchambeau@gmail.com>
2015-10-24 21:43:08 -0700

    2000 satoshis per page turn

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def hello():
     wallet = Wallet('c4e7c925-8a48-4a0f-bf0a-88bae5def1cb', 'Loeras2662#s2n')
 
     # Send from wallet to new address (Receiving address (My Multibit HD for testing), Amount in satoshi, from address)
-    payment = wallet.send('1GKjg6fJjFFjRfJSbSzPSzxzaZZ2iwX1aJ', 100000, from_address='1HKGvNjrMuZ71jkHAFA6e77iPxWM1pranx')
+    payment = wallet.send('1GKjg6fJjFFjRfJSbSzPSzxzaZZ2iwX1aJ', 2000, from_address='1HKGvNjrMuZ71jkHAFA6e77iPxWM1pranx')
 
     print payment.tx_hash
 
